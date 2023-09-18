fb2b673792a5cc3a2bd2ec60f1b7d987878391b1
sismitrii <floguerin156@gmail.com>
2023-09-18 12:35:19 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'calculator';
+  buttonValueList: String[] = [
+    "C",
+    "±",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "X",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    ".",
+    "0",
+    "DEL",
+    "=",
+  ]
+}
+
+// faire un composant bouton 
+// on lui envoie sa valeur 
+// si c'est une valeur numeric le click appelle cette methode et on met telle couleur
+// si c'est une valeur dans un liste /,*,+,-,= on appelle methode operateur et telle couleur
+// si c'est une valeur parmi C,%, ± on appelle methode XXX et telle couleur
